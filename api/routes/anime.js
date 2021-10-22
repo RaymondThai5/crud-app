5fbd07f8f5b053961f6b24699563bf3f3b6a3056
Raymond Thai <rayraythai@gmail.com>
2021-10-21 20:45:16 -0400

    Added Jikan api calls to retrieve anime

--- api/routes/anime.js
+++ api/routes/anime.js
@@ -4,28 +4,34 @@ var mysql = require("mysql");
 
 var router = express.Router();
 
+//Used to modify the string in order to conform to the api's requirements
+//Replaces spaces with slashes, and adds a slash at the end of the string
 const modifyString = (stringValue) => {
     var result = stringValue.replace(/ /g, '/')
     result = result + '/'
     return result
 }
 
+//Connect to database. Localhost is used.
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
-    database: "thedatabase"
+    database: "anime_crud_app"
 })
 
+//Attempt to connect to database
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to database");
 })
 
+//Api test
 router.get("/", function(req, res, next) {
     res.send("Get anime from Jikan API");
 });
 
+//Retrieve the top 50 anime on myanimelist
 router.get("/top-anime", function(req, res) {
     request("https://api.jikan.moe/v3/top/anime/1", function(error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -34,7 +40,8 @@ router.get("/top-anime", function(req, res) {
     })
 });
 
-// 
+//Retrieves a list of anime using a search query parameter inputted
+//by the user
 router.get("/search", function(req, res) {
     request("https://api.jikan.moe/v3/search/anime?q=" + modifyString(req.query.searchQuery) + "&page=1", function (error, response, body) {
         if (!error && response.statusCode == 200) {
--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
+//Api test
 router.get("/", function(req, res, next) {
     res.send("API is working properly");
 });
